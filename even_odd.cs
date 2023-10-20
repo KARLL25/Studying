47e0f882a7f1028dada0007c5c9850f24a5eddd8
daiki <51706588+KARLL25@users.noreply.github.com>
2023-10-20 20:39:46 +0800

    Create even_odd.cs
    counting even/odd in range

---
+++ even_odd.cs
@@ -0,0 +1,36 @@
+using System;
+
+
+namespace even_odd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double even = 0;       //четные
+            double odd = 0;        //нечетные
+
+            Console.WriteLine("Укажите начало лимита");
+            int count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Укажите лимит диапазона:");
+            int limit = int.Parse(Console.ReadLine());
+
+            while (count <= limit)
+            {
+                if (count % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+                count++;
+
+            }
+            Console.WriteLine("Кол-во четных: " + even);
+            Console.WriteLine("Кол-во нечетных: " + odd);
+        }
+    }
+}
