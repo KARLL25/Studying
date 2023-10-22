c4a5bc14c5a25300e843f4caec39f4e07bd35182
daiki <51706588+KARLL25@users.noreply.github.com>
2023-10-22 21:14:45 +0800

    Create triangle_2.cs

---
+++ triangles/triangle_2.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace triangle_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту треугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
