02834c486d5245f6f399975d366127f8ec936412
daiki <51706588+KARLL25@users.noreply.github.com>
2023-10-24 20:53:30 +0800

    4 exercise from task6

---
+++ task6/fourth.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int low = 0;
+            Console.WriteLine("Введите количество значений в массиве: ");
+            int elementsMas = int.Parse(Console.ReadLine());
+            int[] mas = new int[elementsMas];
+
+            Console.WriteLine("Введите значения массива: ");
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = int.Parse(Console.ReadLine());
+           
+            }
+            low = mas.Min();
+            Console.WriteLine(low);
+        }
+
+    }
+}
+
