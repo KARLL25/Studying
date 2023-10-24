1fa7e46f90846710b5a3f51a221167ebdd606da9
daiki <51706588+KARLL25@users.noreply.github.com>
2023-10-24 20:05:10 +0800

    2 exercise from task6

---
+++ task6/Second.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[5];
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = int.Parse(Console.ReadLine());
+                
+                
+            }
+            Console.WriteLine();
+            for (int i = 4;i >= 0; i--) 
+            {
+                Console.WriteLine(mas[i]);
+            }
+        }
+    }
+}
