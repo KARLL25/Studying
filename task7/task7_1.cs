5ffcb5afce0dcdbfdbf278b6ba1193d60944e377
daiki <51706588+KARLL25@users.noreply.github.com>
2023-11-02 22:08:06 +0800

    Create task7_1.cs

---
+++ task7/task7_1.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task7
+{
+    internal class Program
+    {
+        static void PrintLine(char symbol, uint symbolsCount)
+        {
+            
+            for (int i = 0; i < symbolsCount; i++)
+            {
+                Console.WriteLine(symbol);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите символ: ");
+            char symbol = Console.ReadKey().KeyChar;
+
+            Console.WriteLine("\nВведите количество: ");
+            uint symbolsCount = uint.Parse(Console.ReadLine());
+
+            PrintLine(symbol,symbolsCount);
+            Console.ReadLine();
+        }
+    }
+}
